a8b8ece63ccf6f02338b804c6f1d0d3353f7644d
DezZimmerman <dezmondzimmerman@gmail.com>
2026-05-21 19:42:07 -0500

    URL update

---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <iframe
+      src="/games/cookieclicker/index.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+    />
+  );
+}
